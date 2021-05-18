753b53aec3a5b9d1e6ce98624f9eafb7eab8b1a6
Fiach Reid <fiach@globaldataconsortium.com>
2021-05-18 12:18:47 +0200

    removing parallel code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf.Canvas.Parser.Listener;
@@ -29,35 +27,15 @@ namespace PDFExtract
             var ts = DateTime.Now;
             var iPageList = Enumerable.Range(1, pdfDocument.GetNumberOfPages());
             var allText = "";
-            var pageTextSync = new ConcurrentBag<string>();
-            var pageTextParallel = new ConcurrentBag<string>();
+     
 
             foreach (var pageNumber in iPageList)
            {
                var page = pdfDocument.GetPage(pageNumber);
                string text = PdfTextExtractor.GetTextFromPage(page, strategy);
                string processed = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
-               pageTextSync.Add(processed);
-            }
-            
-
-            Parallel.ForEach(iPageList, (pageNumber) => 
-                {
-                    var memoryStreamTemp = new MemoryStream(data);
-                    var pdfDocumentTemp = new PdfDocument(new PdfReader(memoryStreamTemp));
-                    var strategyTemp = new LocationTextExtractionStrategy();
-                    var page = pdfDocumentTemp.GetPage(pageNumber);
-                    string text = PdfTextExtractor.GetTextFromPage(page, strategyTemp);
-                    string processed = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
-                    Console.WriteLine(processed.Length);
-                    pageTextParallel.Add(processed);
-                }
-             );
-             var allTextSync = string.Join("", pageTextSync);
-             var allTextParallel = string.Join("", pageTextParallel);
-            // 		allText.Length	462709	int
-            // 		allText.Length	67813669	int
-
+               allText += processed;
+           }
 
             var elapsed = DateTime.Now - ts;
             Console.WriteLine("seconds" + +elapsed.TotalSeconds); // 7 seconds
